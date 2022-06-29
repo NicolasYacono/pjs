9de983063f2e977d3a76553e3b4b0054a08125da
Nicolas Yacono <nmyacono@gmail.com>
2022-06-29 15:32:52 -0300

    footer y funciones en js agregadas

--- main.js
+++ main.js
@@ -1,40 +1,43 @@
-
-//HACER REPO EN GITHUB//
-
-
-let nombre = prompt("Bienvenido! Ingrese su nombre");
-console.log("Hola "+nombre+" ");
-let edad = parseFloat(prompt("Ingrese su edad"))
-if(edad >= 18){
-    console.log("Acceso aceptado")
-}else{
-    alert("Lo sentimos es menor de edad")
-}
-
-let membresia = prompt("Ingrese el tipo de membresia que desea (sin el numero) \n1 - Premium \n2 - Clasica\n3 - Gratuita \n4 - Ninguna");
-if(membresia != "Ninguna"){
-    if(membresia === "Gratiuta" ){
-        console.log("Contenido de la web y primero asesoramiento gratis")
-    } else if (membresia === "Clasica"){
-        console.log("Beneficios exclusivos, hasta 15gr mensuales, asesoramiento gratis por un mes y contenido de la web")
-        for (let index = 5; index < 16; index = index + 5) {
-            let precio = 1000;
-            console.log("Abono mensual segun cantidad de gramos (5,10 o 15)");
-            console.log("$" + precio * index);
-        }
-    } else if (membresia === "Premium"){
-        console.log("Asesoramiento y cursos gratiutos, hasta 30gr mensuales, beneficios premium, contenido de la web")
-        for (let index = 10; index < 31; index = index + 10) {
-            let precio = 850;
-            console.log("Abono mensual segun cantidad de gramos (10,20 o 30)");
-            console.log("$" + precio * index);
-        }
-    }else{
-        console.log("Lo sentimos la palabra ingresa es incorrecta")
-    }
+/* //PRACTICA
+alert("Bienvenida/o! Le informamos que necesita ser mayor de edad y poseer reprocann para poder inscribirse")
+alert("Ambos seran corroborados con el numero de DNI")
+let dni = parseInt(prompt("Ingrese su DNI"))
+while(dni = ""){
+    console.log("Ingrese su DNI (sin puntos)")
+};
+if(dni >= 45000000){
+    console.log("Lo sentimos es menor de edad")
 } else {
-
-    console.log("Gracias por su visita!")
-}
+    let nombre = prompt("Ingrese su nombre");
+    console.log("Hola "+nombre+" podra acceder a:");
+    function abonoMensual(gramos, precio) {
+        let mensual = "$" + gramos * precio;
+        console.log(mensual);
+    }
+    let membresia = parseInt(prompt("Ingrese el numero del tipo de membresia que desea \n1 - Premium \n2 - Clasica\n3 - Gratuita"));
+        if(membresia === 3 ){
+            console.log("Contenido de la web y primer asesoramiento gratis")
+        } else if (membresia === 2){
+            console.log("Beneficios exclusivos, hasta 15gr mensuales, asesoramiento gratis por un mes y contenido de la web")
+            abonoMensual(5, 900);
+            console.log("Abono mensual por 5gr");
+            abonoMensual(10, 850);
+             console.log("Abono mensual por 10gr");
+            abonoMensual(15, 800);
+            console.log("Abono mensual por 15gr");
+            }
+        else if (membresia === 1){
+            console.log("Asesoramiento y cursos gratiutos, hasta 30gr mensuales, beneficios premium, contenido de la web")
+            abonoMensual(10, 850);
+            console.log("Abono mensual por 10gr");
+            abonoMensual(20, 800);
+             console.log("Abono mensual por 20gr");
+            abonoMensual(30, 750);
+            console.log("Abono mensual por 30gr");
+            }else{
+            console.log("Lo sentimos el numero ingresado es incorrecto")
+            console.log("Gracias por su visita!")
+        } 
+} */
 
 
